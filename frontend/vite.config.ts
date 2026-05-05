668e1372aa6717630d2418dcb65b0585c327f9c8
bysontech <bysontech.jp@gmail.com>
2026-05-06 06:17:10 +0900

    update:setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://share-photo-api.bysontech.jp',
+      '/api': 'https://share-photo-api.bysontech.jp',
     },
   },
 });
